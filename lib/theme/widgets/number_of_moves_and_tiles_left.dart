ad34645c420c8c88b113c96df8acc0d1b0176776
Yijing Huang <hyj891204@gmail.com>
2022-02-24 15:23:17 -0600

    chore: delete unused file

--- lib/theme/widgets/number_of_moves_and_tiles_left.dart
+++ lib/theme/widgets/number_of_moves_and_tiles_left.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_puzzle/colors/colors.dart';
 import 'package:flutter_puzzle/l10n/l10n.dart';
 import 'package:flutter_puzzle/layout/layout.dart';
